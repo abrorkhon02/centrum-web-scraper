81aaa03168423f69bd310356a1dc9c6b47542d07
Abrorkhon Azimov <s8140431@stud.uni-frankfurt.de>
2024-12-17 20:44:32 +0100

    project cleanup and minor fixes

--- puppeteer-server/webScraper.js
+++ puppeteer-server/webScraper.js
@@ -176,7 +176,7 @@ class WebScraper {
     }
   }
 
-  async ensurePageLoad(page, url, retryInterval = 10000) {
+  async ensurePageLoad(page, url, retryInterval = 15000) {
     const startTime = Date.now();
     let loaded = false;
 
